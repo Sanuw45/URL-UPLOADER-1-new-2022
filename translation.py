215193de1553473750e6e900adc7dd34e381f0ef
Sanuw45 <92355575+Sanuw45@users.noreply.github.com>
2022-07-20 08:36:23 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,19 +1,19 @@
 class Translation(object):
 
-    START_TEXT = """Hello,
+    START_TEXT = """Hello මචං,
 
 මම Telegram URL Upload Bot!
 
-<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
+<b>මට ඕනි Direct download URL link එකක් එවන්න, මම ඒක File හෝ Video විදියට upload කරන්නම්</b>
 
-/help for more details..
+උදවු සදහා /help ..
 
 Film group - @film_with_review
 """
 
-    HELP_USER = """Hai I'am a URL Uploader bot..
+    HELP_USER = """මම URL Uploader bot..
     
-1. Send url (Link | New Name with Extension).
+1.  url එක එවන්න (Link | New Name with Extension).
 2. Send Custom Thumbnail (Optional).
 3. Select the button.
    SVideo - Give File as video with Screenshots
@@ -24,7 +24,7 @@ Film group - @film_with_review
 Support Group : @film_with_review
 """
 
-    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
+    FORMAT_SELECTION = """ඔයාට ඕනි format එක තෝරන්න: <a href='{}'>file size might be approximate</a>
     
 Send your custum thumbnail if required.
 You can use /deletethumbnail to delete the auto-generated thumbnail."""
@@ -37,12 +37,12 @@ URL | newfilename | username | password"""
     
     UPLOAD_START = "Uploading now..👻👻👻"
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds. මාව hospital දාන්න අයියෝ."
 
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nමේ හුත්තො Telegram එකට upload කරන්න පුළුවන් උපරිම 1.96 GB "
 
     CUSTOM_CAPTION_UL_FILE = " "
 
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "මේකෙ මුකුත් නෑ හුත්තෝ \n<b>YouTubeDL</b> said: {}"
